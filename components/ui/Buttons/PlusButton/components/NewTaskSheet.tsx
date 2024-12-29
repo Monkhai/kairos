df90341e94136bb3b09ba6d50df6f156f8b343c0
Yohai Wiener <yohaiwiener@gmail.com>
2024-12-29 11:25:16 +0200

    fix: reset new task sheet state on close

--- components/ui/Buttons/PlusButton/components/NewTaskSheet.tsx
+++ components/ui/Buttons/PlusButton/components/NewTaskSheet.tsx
@@ -9,9 +9,10 @@ import { createTask } from '@/server/tasks/queries'
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet'
 import { useMutation } from '@tanstack/react-query'
 import { useAtom } from 'jotai'
-import React from 'react'
+import React, { useRef } from 'react'
 import { Keyboard, Pressable, useColorScheme, View } from 'react-native'
 import Button from '../../TextButton'
+import { InputRef } from '@/components/ui/inputs/InputText'
 
 interface Props {
   bottomSheetRef: React.RefObject<BottomSheet>
@@ -23,6 +24,18 @@ export default function NewTaskSheet({ bottomSheetRef }: Props) {
   const [hours, setHours] = React.useState(0)
   const [minutes, setMinutes] = React.useState(0)
   const [searchQuery] = useAtom(taskSearchQueryAtom)
+  const nameRef = useRef<InputRef>(null)
+  const descriptionRef = useRef<InputRef>(null)
+
+  const resetState = () => {
+    setTaskName('')
+    setTaskDescription('')
+    nameRef.current?.reset()
+    descriptionRef.current?.reset()
+    setHours(0)
+    setMinutes(0)
+  }
+
   const { mutate: createTaskMutation, isPending } = useMutation({
     mutationFn: async ({ description, duration, title }: { title: string; description: string; duration: number }) =>
       await createTask(title, description, duration),
@@ -33,7 +46,7 @@ export default function NewTaskSheet({ bottomSheetRef }: Props) {
       bottomSheetRef.current?.close()
     },
 
-    onError: (error) => {
+    onError: error => {
       console.error(error)
     },
   })
@@ -47,20 +60,27 @@ export default function NewTaskSheet({ bottomSheetRef }: Props) {
   const disabled = taskName?.length === 0 || taskDescription?.length === 0 || duration === 0
 
   return (
-    <CustomBottomSheet bottomInset={0} bottomSheetRef={bottomSheetRef} snapPoints={['60%']}>
+    <CustomBottomSheet
+      onClose={() => {
+        resetState()
+      }}
+      bottomInset={0}
+      bottomSheetRef={bottomSheetRef}
+      snapPoints={['60%']}
+    >
       <BottomSheetView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%' }}>
         <Title label={'New Task'} />
         <Pressable onPress={() => Keyboard.dismiss()} style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '80%' }}>
           <>
             <View style={{ gap: 16, width: '100%' }}>
-              <InputField placeholder='name' value={taskName} onChangeText={setTaskName} />
-              <InputField placeholder='description' value={taskDescription} onChangeText={setTaskDescription} />
+              <InputField ref={nameRef} placeholder="name" value={taskName} onChangeText={setTaskName} />
+              <InputField ref={descriptionRef} placeholder="description" value={taskDescription} onChangeText={setTaskDescription} />
             </View>
             <DurationPicker inModal hours={hours} minutes={minutes} setHours={setHours} setMinutes={setMinutes} />
           </>
         </Pressable>
         <View style={{ paddingBottom: 64 }}>
-          <Button disabled={disabled} isLoading={isPending} label='Add task' onPress={handleCreateTask} />
+          <Button disabled={disabled} isLoading={isPending} label="Add task" onPress={handleCreateTask} />
         </View>
       </BottomSheetView>
     </CustomBottomSheet>
